69966a2f77bf3bc512023112ec7073263da2bfee
SatanWoo <applewuziqi@gmail.com>
2012-02-16 15:36:32 +0800

    Wu:Hide remove button and drag file label

--- SRC/BubblesOnMac/DragFileViewController.m
+++ SRC/BubblesOnMac/DragFileViewController.m
@@ -10,6 +10,7 @@
 
 @implementation DragFileViewController
 @synthesize imageView = _imageView;
+@synthesize label = _label;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
